a195d195ba6d5475fe6111cacefdf414555c5150
Vijay <vijaysathappan.manickam@gmail.com>
2026-04-04 19:59:58 +0530

    update

--- api/query.py
+++ api/query.py
@@ -1,7 +1,8 @@
 import os
 import sys
 
-# Add the project root to sys.path so we can import llm_service
-sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+# Add the llm_service directory directly to sys.path
+# This allows 'main.py' to find 'agent.py', 'database.py', etc.
+sys.path.append(os.path.join(os.path.dirname(__file__), "..", "llm_service"))
 
-from llm_service.main import app
+from main import app
